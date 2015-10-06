6fbd64b662734a55a6baf0a79dbba90fe38a7c46
Noah Yarian <noah.yarian@gmail.com>
2015-10-05 23:24:54 -0500

    created draw squares function. It is only creating one but the effect is really sweet! Totally planned... abspos containers for layering.

--- src/squares.js
+++ src/squares.js
@@ -1,3 +1,57 @@
+$(function() {
+  console.log('jQuery and the document are loaded');
+  updateWindowSize();
+});
+
+// listeners
+$(window).resize(function() {
+  updateWindowSize();
+});
+
+$('#trySquares').click(drawSquares);
+
+
+///////////
+
+function updateWindowSize() {
+  var width = $(window).width();
+  var height = $(window).height();
+  $('#width').text('width: ' + width);
+  $('#height').text('height: ' + height);
+}
+
+function drawSquares() {
+  var width = $(window).width();
+  var height = $(window).height();
+  var square = $('#square').val();
+  var squareMin = $('#squareMin').val();
+  var squareMax = $('#squareMax').val();
+  var gutter = $('#gutter').val();
+  var gutterMin = $('#gutterMin').val();
+  var gutterMax = $('#gutterMax').val();
+
+  var rows = (width - gutter) / (square + gutter);
+  var cols = (width - gutter) / (square + gutter);
+  var num = rows * cols;
+
+  var squareDiv;
+  for (var i = 0; i < num; i++) {
+    squareDiv = document.createElement('div');
+    $(squareDiv)
+      .addClass('drawnSquare')
+      .css({
+        'height': square,
+        'width': square,
+        'margin-left': gutter,
+        'margin-top': gutter,
+        'background': randomColor({luminosity: 'bright'}),
+        'border': '5px solid ' + randomColor({luminosity: 'bright'})
+      });
+    $('.squares').append(squareDiv);
+  }
+}
+
+///////////
 function checkSquareFit(windowWidth, windowHeight, squareSide, gutter) {
   if (
     (windowWidth - gutter) % (squareSide + gutter) === 0 &&
@@ -7,7 +61,7 @@ function checkSquareFit(windowWidth, windowHeight, squareSide, gutter) {
     console.log('squareSide: ' + squareSide + 'px');
     console.log('gutter: ' + gutter + 'px');
     console.log('columns: ' + (windowWidth - gutter) / (squareSide + gutter));
-    console.log('rows: ' + (windowHeight - gutter) / (squareSide + gutter));
+    console.log('rows: ' + (windowWidth - gutter) / (squareSide + gutter));
     return true;
   } else {
     console.log('no dice.')

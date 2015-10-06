f3c79de545587ced4b54cbd3d46b0ef94d98dcde
Noah Yarian <noah.yarian@gmail.com>
2015-10-05 20:06:30 -0500

    added functions to try different gutter or square sizes to see if it will make it fit perfectly in a rectangle

--- src/main.js
+++ src/main.js
@@ -111,26 +111,6 @@ function getColors() {
 //   return hue;
 // }
 
-function trySquareSize(windowWidth, windowHeight, squareSize, gutter) {
-  if (
-    (windowWidth - gutter) % (squareSize + gutter) === 0 &&
-    (windowHeight - gutter) % (squareSize + gutter) === 0
-  ) {
-    console.log('it works!');
-    console.log('squareSize: ' + squareSize + 'px');
-    console.log('gutter: ' + gutter + 'px');
-    console.log('columns: ' + (windowWidth / squareSize));
-    console.log('rows: ' + (windowHeight / squareSize));
-  } else {
-    console.log('no dice.')
-    console.log('squareSize: ' + squareSize + 'px');
-    console.log('gutter: ' + gutter + 'px');
-    console.log('columns: ' + (windowWidth - gutter) / (squareSize + gutter));
-    console.log('rows: ' + (windowHeight - gutter) / (squareSize + gutter));
-  }
-}
-
-
 function makeElements(size) {
   var width = $(window).width();
   var height = $(window).height();
@@ -161,4 +141,70 @@ function makeElements(size) {
 
 function clearElements() {
   $('.container').html('');
+}
+
+// Fit Squares!
+/////////////////
+
+function trySquareSize(windowWidth, windowHeight, squareSize, gutter) {
+  if (
+    (windowWidth - gutter) % (squareSize + gutter) === 0 &&
+    (windowHeight - gutter) % (squareSize + gutter) === 0
+  ) {
+    console.log('it works!');
+    console.log('squareSize: ' + squareSize + 'px');
+    console.log('gutter: ' + gutter + 'px');
+    console.log('columns: ' + (windowWidth - gutter) / (squareSize + gutter));
+    console.log('rows: ' + (windowHeight - gutter) / (squareSize + gutter));
+    return true;
+  } else {
+    console.log('no dice.')
+    console.log('squareSize: ' + squareSize + 'px');
+    console.log('gutter: ' + gutter + 'px');
+    console.log('columns: ' + (windowWidth - gutter) / (squareSize + gutter));
+    console.log('rows: ' + (windowHeight - gutter) / (squareSize + gutter));
+    return false;
+  }
+}
+
+function varyGutter(windowWidth, windowHeight, squareSize, gutter, minGutter, maxGutter) {
+  if (trySquareSize(windowWidth, windowHeight, squareSize, gutter)) {
+    console.log('no need to vary the gutter size.');
+  } else {
+    for (var i = minGutter; i <= maxGutter; i++) {
+      if (i === gutter) {
+        continue;
+      } else if (trySquareSize(windowWidth, windowHeight, squareSize, i)) {
+        console.log('success! A gutter of ' + i + 'px works.');
+        return true;
+      } else {
+        console.log('a gutter of ' + i + " won't work");
+      }
+    }
+    console.log('no suitable gutter size found within range');
+    return false;
+  }
+}
+
+function varySquareSize(windowWidth, windowHeight, squareSize, gutter, minSquareSize, maxSquareSize) {
+  if (!minSquareSize) {
+    minSquareSize = 1;
+    maxSquareSize = windowWidth > windowHeight ? windowHeight : windowWidth;
+  }
+  if (trySquareSize(windowWidth, windowHeight, squareSize, gutter)) {
+    console.log('no need to vary the square size.');
+  } else {
+    for (var i = maxSquareSize; i >= minSquareSize; i--) {
+      if (i === squareSize) {
+        continue;
+      } else if (trySquareSize(windowWidth, windowHeight, i, gutter)) {
+        console.log('success! A squareSize of ' + i + 'px works.');
+        return true;
+      } else {
+        console.log('a squareSize of ' + i + " won't work");
+      }
+    }
+    console.log('no suitable squareSize found within range');
+    return false;
+  }
 }

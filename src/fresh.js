742a1db3911a72b95b6d70f8e145fec696a1a6e8
Noah Yarian <noah.yarian@gmail.com>
2015-10-07 20:46:26 -0500

    settled on a few sizes that allow for lots of square sizes

--- src/fresh.js
+++ src/fresh.js
@@ -5,10 +5,6 @@
 // What is my MVP for these dancing squares?
 // - appear centered in the window
 // - work in a few different window sizes
-// -- 1440x900
-// -- 1440x727
-// -- 1000x700
-// -- 650x400
 // - be colorful
 // - change visually according to beat information
 // - change without lagging noticeably
@@ -22,21 +18,26 @@ $(function() {
 function setContainerSize() {
   var width = $(window).width();
   var height = $(window).height();
-  var w = 650;
-  var h = 400;
+  var w = 720;
+  var h = 480;
 
   if (width >= 1440 && height >= 900) {
     w = 1440;
     h = 900;
-  } else if (width >= 1440 && height >= 727) {
+  } else if (width >= 1440 && height >= 720) {
     w = 1440;
-    h = 727;
-  } else if (width >= 1000 && height >= 700) {
-    w = 1000;
-    h = 700;
+    h = 720;
+  } else if (width >= 1260 && height >= 720) {
+    w = 1260;
+    h = 720;
   }
   $('.squares').css({width: w, height: h});
 }
+
+// -- 1440x900 -- 10,12,15,18,20,30,36,45,60,90,180
+// -- 1440x720 -- 10,12,15,16,18,20,24,30,36,40,45,48,60,72,80,90,120,144,180,240,360,720
+// -- 1260x720 -- 10,12,15,18,20,30,36,45,60,90,180
+// -- 720x480 -- 10,12,15,16,20,24,30,40,48,60,80,120,240
 
 function drawSquares() {
 
